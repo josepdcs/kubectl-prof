81761281977eb94dd0535e10e4ef2c5bd814f1a0
Josep Damià Carbonell Seguí <josepdcs@ext.inditex.com>
2025-12-29 10:46:33 +0100

    feat(GH-84): Rust profiler job with cargo-flamegraph

--- test/stupid-apps/rust/target/debug/deps/rust-84d03fd6ea08dbe1.d
+++
@@ -1,5 +0,0 @@
-/home/josepdcs/Desarrollo/Personal/Profiling/kubectl-prof/test/stupid-apps/rust/target/debug/deps/rust-84d03fd6ea08dbe1.d: main.rs
-
-/home/josepdcs/Desarrollo/Personal/Profiling/kubectl-prof/test/stupid-apps/rust/target/debug/deps/rust-84d03fd6ea08dbe1: main.rs
-
-main.rs:
--- test/stupid-apps/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/josepdcs/Desarrollo/Personal/Profiling/kubectl-prof/test/stupid-apps/rust/target/debug/rust: /home/josepdcs/Desarrollo/Personal/Profiling/kubectl-prof/test/stupid-apps/rust/main.rs
